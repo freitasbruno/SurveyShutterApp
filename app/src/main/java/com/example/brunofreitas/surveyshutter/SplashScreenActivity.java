5050336b88fde66ac0d3b2db9de0a2d4a221dccc
Bruno Freitas <freitascbruno@gmail.com>
2016-03-31 12:08:43 +0100

    Added Project List Activity template Linked Splash screen to ProjectListActivity through skipLogin button

--- app/src/main/java/com/example/brunofreitas/surveyshutter/SplashScreenActivity.java
+++ app/src/main/java/com/example/brunofreitas/surveyshutter/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.brunofreitas.surveyshutter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    Intent intent;
+
+    /** Called when the user clicks the Skip button */
+    public void startProjectListActivity(View view) {
+        intent = new Intent(this, ProjectListActivity.class);
+        startActivity(intent);
     }
 }
